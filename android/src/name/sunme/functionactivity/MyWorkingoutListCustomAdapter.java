47627214c2e7ff12304548358e98d128159c5bd2
silvia UEO <mjisnmd@gmail.com>
2015-02-15 19:29:01 +0900

    리스트뷰인리스트뷰 높이조절 남음

--- android/src/name/sunme/functionactivity/MyWorkingoutListCustomAdapter.java
+++ android/src/name/sunme/functionactivity/MyWorkingoutListCustomAdapter.java
@@ -1,19 +1,27 @@
 package name.sunme.functionactivity;
   
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import name.sunme.maindrawbar.MyDrawerItem;
 import name.sunme.maindrawbar.R;
+import name.sunme.seniorfit.FitApiDataClass;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class MyWorkingoutListCustomAdapter extends ArrayAdapter<MyWorkingoutItem>{
+	String TAG = "MyWorkingoutListCustomAdapter";
 	Context context;
 	int resource;
 	ArrayList<MyWorkingoutItem> data;
@@ -37,6 +45,34 @@ public class MyWorkingoutListCustomAdapter extends ArrayAdapter<MyWorkingoutItem
         TextView title = (TextView) listItem.findViewById(R.id.myworkingout_title_row);
         title.setText(data.get(position).title);  
         
+        
+        
+        //리스트뷰 인 리스트뷰를 위해!
+        ListView listview = (ListView)listItem.findViewById(R.id.myworkingout_subtitle);
+        ArrayList<HashMap<String, String>> listdata = new ArrayList<HashMap<String, String>>(); 
+        ArrayList<FitApiDataClass> fads = data.get(position).fads;
+        Log.d(TAG, "타이틀 : "+title + ", 하위메뉴 : "+fads.size() + "개");
+        for(int i=0 ; i<fads.size(); i++) {
+        	HashMap<String, String> hm = new HashMap<String, String>();
+        	hm.put("title", fads.get(i)._subMenuTitle);
+        	listdata.add(hm);
+        	Log.d(TAG, "하위메뉴 : "+fads.get(i)._subMenuTitle);
+        	//데이터에서 타이틀만뽑아야지 일단.
+        }
+        SimpleAdapter simpleadapter = new SimpleAdapter(
+				context,  
+				listdata,
+        		R.layout.activity_my_workingout_row_row,
+        		new String[]{"title"}, 
+        		new int[]{R.id.myworkingout_title_row_row});
+        listview.setAdapter(simpleadapter);
+        
+        
+         
+        //listview.getLayoutParams().height = listdata.size() * 90;
+        //listview.requestLayout();
+		
+		////////
         return listItem;
     }
 
